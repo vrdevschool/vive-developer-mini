108784e45b96d3a31ff109b6c265d4ccd4008bc8
eljaydub <lee.wasilenko@gmail.com>
2016-03-12 21:50:26 -0800

    End of 'Throw an Object Parented to the Controller'

--- Assets/Scripts/PickupParent.cs
+++ Assets/Scripts/PickupParent.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 [RequireComponent(typeof(SteamVR_TrackedObject))]
 public class PickupParent : MonoBehaviour {
 
     SteamVR_TrackedObject trackedObj;
     SteamVR_Controller.Device device;
+
+    public Transform sphere;
 
     void Awake () {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
@@ -43,5 +46,47 @@ public class PickupParent : MonoBehaviour {
         {
             Debug.Log("You activated PressUp on the Trigger");
         }
+
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Debug.Log("You activated PressUp on the Touchpad");
+            sphere.transform.position = Vector3.zero;
+            sphere.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            sphere.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
+    }
+
+    void OnTriggerStay (Collider col)
+    {
+        Debug.Log("You have collided with " + col.name + " and activated OnTriggerStay");
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have collided with " + col.name + " while holding down Touch");
+            col.attachedRigidbody.isKinematic = true;
+            col.gameObject.transform.SetParent(gameObject.transform);
+        }
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have released Touch while colliding with " + col.name);
+            col.gameObject.transform.SetParent(null);
+            col.attachedRigidbody.isKinematic = false;
+
+            tossObject(col.attachedRigidbody);
+        }
+    }
+
+    void tossObject(Rigidbody rigidBody)
+    {
+        Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+        if (origin != null)
+        {
+            rigidBody.velocity = origin.TransformVector(device.velocity);
+            rigidBody.angularVelocity = origin.TransformVector(device.angularVelocity);
+        } else
+        {
+            rigidBody.velocity = device.velocity;
+            rigidBody.angularVelocity = device.angularVelocity;
+        }
+        
     }
 }
