f3ccb347bf939fcd142af65da05f27b90f903637
eljaydub <lee.wasilenko@gmail.com>
2016-04-09 15:12:27 -0700

    Add ParentFixedJoint Script

---
+++ Assets/Scripts/ParentFixedJoint.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class ParentFixedJoint : MonoBehaviour {
+
+    public Rigidbody rigidBodyAttachPoint;
+    public Transform sphere;
+
+    SteamVR_TrackedObject trackedObj;
+    SteamVR_Controller.Device device;
+
+    FixedJoint fixedJoint;
+
+    void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+	
+	void FixedUpdate () {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Debug.Log("You activated PressUp on the Touchpad");
+            sphere.transform.position = Vector3.zero;
+            sphere.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            sphere.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        Debug.Log("You have collided with " + col.name + " and activated OnTriggerStay");
+        if (fixedJoint == null && device.GetTouch(SteamVR_Controller.ButtonMask.Trigger)) {
+            fixedJoint = col.gameObject.AddComponent<FixedJoint>();
+            fixedJoint.connectedBody = rigidBodyAttachPoint;
+        }
+        else if (fixedJoint != null && device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            GameObject go = fixedJoint.gameObject;
+            Rigidbody rigidBody = go.GetComponent<Rigidbody>();
+            Object.Destroy(fixedJoint);
+            fixedJoint = null;
+            tossObject(rigidBody);
+        }
+    }
+
+    void tossObject(Rigidbody rigidBody)
+    {
+        Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+        if (origin != null)
+        {
+            rigidBody.velocity = origin.TransformVector(device.velocity);
+            rigidBody.angularVelocity = origin.TransformVector(device.angularVelocity);
+        }
+        else
+        {
+            rigidBody.velocity = device.velocity;
+            rigidBody.angularVelocity = device.angularVelocity;
+        }
+
+    }
+}
